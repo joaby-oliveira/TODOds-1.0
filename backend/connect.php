0afa2c4f91ce97e6aa3f6ca172430f056288bd37
Joaby Oliveira <joaby.santos@etec.sp.gov.br>
2020-03-30 01:24:32 -0300

    :bug: FIXED: connect.php

--- backend/connect.php
+++ backend/connect.php
@@ -4,7 +4,9 @@ $host = "sql201.epizy.com";
 $user = "epiz_25058552";
 $pass = "zOpAv9fKPeOOpo";
 $banco = "epiz_25058552_devisate";
+
 $conexao = mysqli_connect ($host, $user, $pass) or die (mysqli_connect_error());
+
 if(isset($conexao)){
     echo "DEU CERTO PARSA!";
 }
