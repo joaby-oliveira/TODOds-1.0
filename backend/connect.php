7abe3106e0d14563a0baf904df5d56441a631d9f
Gabriel <gabriel.rodrigues313@etec.sp.gov.br>
2020-03-30 00:18:06 -0300

    :bug: FIXED: Error function php

--- backend/connect.php
+++ backend/connect.php
@@ -1,11 +1,16 @@
 <?php
+
 $host = "sql201.epizy.com";
 $user = "epiz_25058552";
 $pass = "zOpAv9fKPeOOpo";
 $banco = "epiz_25058552_devisate";
-$conexao = mysqli_connect ($host, $user, $pass) or die (mysql_error());
+
+$conexao = mysqli_connect ($host, $user, $pass) or die (mysqli_connect_error());
+
 if(isset($conexao)){
     echo "DEU CERTO PARSA!";
 }
+
 mysqli_set_charset($conexao, "utf8");
+
 ?>
