1cc35b783f7667f52887e52be65e2dcb2eb87f14
Joaby Oliveira <joaby.santos@etec.sp.gov.br>
2020-04-01 00:37:46 -0300

    :art: EDIT: connect.php Using PDO PHP to make the database connection

--- backend/connect.php
+++ backend/connect.php
@@ -1,16 +1,13 @@
 <?php
-
-$host = "sql201.epizy.com";
-$user = "epiz_25058552";
-$pass = "zOpAv9fKPeOOpo";
-$base = "epiz_25058552_devisate";
-
-$connection = mysqli_connect ($host, $user, $pass, $base) or die (mysqli_connect_error());
-
-if(isset($connection)){
-    echo "DEU CERTO PARSA!";
+function novaConexao($base, $server, $user, $password){
+    try{
+        $connect = new PDO("mysql:host=$server;dbname=$base", $user, $password);
+        echo "DEU CERTO";
+    }catch(PDOException $e){
+        echo "Erro: " . $e->getMessage();
+    }
+    return $connect;   
 }
-
-mysqli_set_charset($connection, "utf8");
+$conexao = novaConexao("cursophp", "localhost", "root", "");
 
 ?>
