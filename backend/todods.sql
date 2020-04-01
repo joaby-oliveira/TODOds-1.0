26bbb77856a76d8362493b38a831f4e707fab15a
Gabriel <gabriel.rodrigues313@etec.sp.gov.br>
2020-04-01 00:28:54 -0300

    :bug: FIXED: todods.sql Database FK error

--- backend/todods.sql
+++ backend/todods.sql
@@ -1,3 +1,13 @@
+CREATE DATABASE todods;
+
+USE todods; 
+
+CREATE TABLE class (
+  classCode INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  class ENUM("1º ETIM DS", "2º ETIM DS", "3º INFO", "1º ADM", "2º ADM", "3º ADM"),
+  team ENUM('A','B') NOT NULL
+);
+
 CREATE TABLE events (
     eventCode INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     date DATETIME NOT NULL,
@@ -5,13 +15,8 @@ CREATE TABLE events (
     description TEXT NOT NULL,
     status ENUM('0', '1'),
     photo TEXT NOT NULL,
-    FK_class INT NOT NULL
-); 
-
-CREATE TABLE class (
-  classCode INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-  class ENUM("1º ETIM DS", "2º ETIM DS", "3º INFO", "1º ADM", "2º ADM", "3º ADM"),
-  team ENUM('A','B') NOT NULL
+    FK_classCode INT NOT NULL,
+    FOREIGN KEY (FK_classCode) REFERENCES class(classCode)
 );
 
 CREATE TABLE users (
@@ -20,9 +25,9 @@ CREATE TABLE users (
   lastname VARCHAR(25) NOT NULL,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(32) NOT NULL, 
-  gender ENUM('M', 'F', 'O') NOT NULL  
-  -- FK_class INT NOT NULL,
-  -- FOREIGN KEY (FK_class) REFERENCES class(classCode)
+  gender ENUM('M', 'F', 'O') NOT NULL,  
+  FK_classCode INT NOT NULL,
+  FOREIGN KEY (FK_classCode) REFERENCES class(classCode)
 );
 
 
