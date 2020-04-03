8ee26fc761e0460e9f46b2249775776446650cd3
Joaby Oliveira <joaby.santos@etec.sp.gov.br>
2020-04-03 00:50:52 -0300

    :art: EDITED: register.php

--- backend/register.php
+++ backend/register.php
@@ -9,13 +9,20 @@ $lastname = $_POST['last'];
 $gender = $_POST['gender'];
 $class = 1;
 
+echo $email . "<br>";
+echo $pass . "<br>";
+echo $firstname . "<br>";
+echo $lastname . "<br>";
+echo $gender . "<br>";
+echo $class . "<br>";
+
 $query = "SELECT email FROM users WHERE email = '$email'";
 $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-print_r($result);
 if(mysqli_num_rows($result) == 0){
     $query = "INSERT INTO users(firstname, lastname, email, password, gender, classCode)
     VALUES('$firstname', '$lastname', '$email', '$pass', '$gender', $class)";
     $resultado = mysqli_query($connection, $query);
+    print_r($result);
 }else{
     echo "Erro: " . mysqli_connect_error();
 }
