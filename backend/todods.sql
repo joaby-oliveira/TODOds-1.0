85d21bc5ef3e6920d3029d1ca20625046a59839c
Joaby Oliveira <joaby.santos@etec.sp.gov.br>
2020-04-03 02:38:38 -0300

    :art: todods.sql Creating photo table and changing the furter tables

--- backend/todods.sql
+++ backend/todods.sql
@@ -1,24 +1,27 @@
+DROP DATABASE IF EXISTS todods;
 CREATE DATABASE IF NOT EXISTS todods;
 
 USE todods; 
-
+DROP TABLE IF EXISTS class;
 CREATE TABLE IF NOT EXISTS class (
   classCode INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  class ENUM("1º ETIM DS", "2º ETIM DS", "3º INFO", "1º ADM", "2º ADM", "3º ADM"),
-  team ENUM('A','B') NOT NULL
+  className VARCHAR(20) NOT NULL UNIQUE,
+  schoolName VARCHAR(80) NOT NULL
 );
 
+DROP TABLE IF EXISTS events;
 CREATE TABLE IF NOT EXISTS events (
   eventCode INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   date DATETIME NOT NULL,
   subject VARCHAR(100) NOT NULL, 
   description TEXT NOT NULL,
   status ENUM('0', '1'),
-  photo TEXT NOT NULL,
   classCode INT UNSIGNED NOT NULL,
+  turma set('A', 'B', 'AB'),
   CONSTRAINT FK_classCode FOREIGN KEY (classCode) REFERENCES class(classCode)
 );
 
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
   userCode INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   firstname VARCHAR(25) NOT NULL,
@@ -28,4 +31,38 @@ CREATE TABLE IF NOT EXISTS users (
   gender ENUM('M', 'F', 'O') NOT NULL,  
   classCode INT UNSIGNED NOT NULL,
   CONSTRAINT FOREIGN KEY (classCode) REFERENCES class(classCode)
+);
+
+DROP TABLE IF EXISTS photos;
+CREATE TABLE IF NOT EXISTS photos (
+  photoCode INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  file VARCHAR(40) NOT NULL,
+  date DATETIME NOT NULL,
+  eventCode INT UNSIGNED NOT NULL,
+  CONSTRAINT FOREIGN KEY (eventCode) REFERENCES events(eventCode)
+);
+
+INSERT INTO class(schoolName, className)
+VALUES (
+  'Etec Antônio Devisate',
+  '1° ETIM DS'
+);
+
+INSERT INTO events(date, subject, description, status, classCode)
+ VALUES (
+  '2020-04-03',
+  'Matemática',
+  'Tarefa sobre matrizes',
+  '0',
+  1
+);
+
+INSERT INTO users (firstname, lastname, email, password, gender, classCode)
+VALUES (
+  'Joaby',
+  'Oliveira',
+  'joaby@gmail.com',
+  'tototo',
+  'M',
+  1
 );
