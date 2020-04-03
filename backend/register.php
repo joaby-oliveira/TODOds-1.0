0d89ed5ce5246022d4624d2f9b02754482c36d3a
Gabriel <gabriel.rodrigues313@etec.sp.gov.br>
2020-04-02 23:49:17 -0300

    :arrow_double_up: ADDED: register.php Adding register file

---
+++ backend/register.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once 'backend/connect.php';
+
+$email = $_POST['email'];
+$pass = MD5($_POST['password']);
+$firstname = $_POST['first'];
+$lastname = $_POST['last'];
+$gender = $_POST['gender'];
+$class = 1;
+
+$query = "SELECT email FROM users WHERE email = '$email'";
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+
+if(mysqli_num_rows($result) == 0){
+    $query = "INSERT INTO users(firstname, lastname, email, password, gender, classCode)
+    VALUES('$firstname', '$lastname', '$email', '$password', '$gender', '$class)";
+}else{
+    echo "Erro: " . $connection;
+}
+
+?>
