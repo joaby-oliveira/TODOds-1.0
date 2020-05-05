c4c1b9c92d8b10b420e84a0aacac17324e4bcb82
Joaby Oliveira <joaby.santos@etec.sp.gov.br>
2020-05-04 21:40:42 -0300

    :memo: RENAMED:inserts.php & eventRegister.php

---
+++ register.php
@@ -0,0 +1,8 @@
+<form action="backend/register.php" method="post">
+    <input type="text" name="first" placeholder="firstname">
+    <input type="text" name="last" placeholder="lastname">
+    <input type="text" name="email" placeholder="email">
+    <input type="text" name="password" placeholder="password">
+    <input type="text" name="gender" placeholder="gender">
+    <input type="submit">
+</form>
